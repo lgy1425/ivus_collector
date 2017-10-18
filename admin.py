0a6f82df9904c374941086bc9670711b818f8338
mac <mac-pc@macui-MacBook-Pro-2.local>
2017-10-18 22:00:51 +0900

    first pages

--- admin.py
+++ admin.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,render_template,current_app,url_for,g,session,redirect,make_response,request
-
+import random
+import os
+import json
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
-app.config['TEMPLATE_AUTO_RELOAD'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.jinja_env.cache = None
-
+app.config["UPLOAD_FOLDER"] = "static/eye"
 
 
 @app.route("/")
@@ -17,6 +19,36 @@ def index():
 def before_request():
     # g.db = dbconnect()
     print ""
+
+
+@app.route("/upload_eye",methods=["POST"])
+def upload_eye() :
+
+
+    for f in request.files :
+        eye = request.files[f]
+        filename = eye.name
+        index = filename.rfind('.')
+        print filename
+        extension = filename[index + 1:]
+        fundusname = filename[0:index]
+
+   
+    save_path = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+    eye.save(save_path)
+
+    image_path = "./static/eye/" +filename
+
+
+    
+    optical_disk = random.randint(0,2)
+    vessel_ratio = random.randint(0,2)
+    bleeding = random.randint(0,2)
+    retina = random.randint(0,2)
+    etc = random.randint(0,2)
+ 
+    result = {"eye_path":image_path,"vessel_ratio":vessel_ratio,"optical_disk":optical_disk,"bleeding":bleeding,"retina":retina,"etc":etc,"fundus_name":fundusname}
+    return json.dumps(result)
 
 if __name__=="__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,6 @@
-
-id_list = [1]
-
 $(".nav-tabs").on("click", "a", function (e) {
     e.preventDefault();
-    if (!$(this).hasClass('add-contact')) {
+    if (!$(this).hasClass('add-fundus')) {
         $(this).tab('show');
     }
 })
@@ -19,7 +16,177 @@ e.preventDefault();
 
 var id = $(".nav-tabs").children().length;
 var tabId = 'fundus_' + id;
-$(this).closest('li').before('<li><a href="#fundus_' + id + '">New Fundus</a> <span> x </span></li>');
-$('.tab-content').append('<div class="tab-pane" id="' + tabId + '">Contact Form: New Contact ' + id + '</div>');
-$('.nav-tabs li:nth-child(' + id + ') a').click();
+$(this).closest('li').before('<li><a href="#fundus_' + id + '" id="tab_' + id + '">New Fundus</a> <span> x </span></li>');
+
+var html = generate_new_tab(id)
+
+$('.tab-content').append(html);
+
+
+
+$('#tab_'+id).click();
 });
+
+
+function generate_new_tab(id) {
+	var str = "<div class='tab-pane active' id='fundus_id_number'>\
+                <input type='file' id='uploader_id_number' class='uploader' style='visibility: hidden;height:0px;' fundus_id='id_number'>\
+                <div class='row'>\
+                    <div class='col-md-8'>\
+                        <div class='dragdrop' fundus_id='id_number' id='dragdrop_id_number'>\
+                            <img src='' class='eye_img' id='eye_img_id_number'>\
+                            <p>Drag&Drop<br><br>Fundus Photo</p>\
+                        </div>\
+                        <div class='upload_here' fundus_id='id_number'>\
+                            <p>or Upload here</p>\
+                        </div>\
+                    </div>\
+                    <div class='col-md-4'>\
+                        <div class='noon'>\
+                            <p class='drnoon'>&lt; Dr.Noon &gt;</p>\
+                            <p class='clinic_note clinic_note_title' id='note_title_id_number'>&nbsp<p>\
+                            <p class='clinic_note' id='optical_disk_id_number'>&nbsp<p>\
+                            <p class='clinic_note' id='vessel_ratio_id_number'>&nbsp<p>\
+                            <p class='clinic_note' id='bleeding_id_number'>&nbsp<p>\
+                            <p class='clinic_note' id='retina_id_number'>&nbsp<p>\
+                            <p class='clinic_note' id='etc_id_number'>&nbsp<p>\
+                            <div class='detail_report_btn' fundus_id='id_number'><p>Detail Report</p></div>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+            "
+
+    str_replaced = str.replace(new RegExp('id_number', 'g'),id.toString());
+    return str_replaced
+}
+
+$(function() {
+
+	var obj = $(".dragdrop");
+
+	var id = ""
+
+	$(document).on('dragenter', '.dragdrop', function (e) { 
+
+		e.stopPropagation();
+        e.preventDefault();
+	    id = $(this).attr("fundus_id");
+	    
+	});
+
+	$(document).on('dragover', '.dragdrop', function (e) {
+
+	    e.stopPropagation();
+        e.preventDefault();
+	});
+
+	$(document).on('drop', '.dragdrop', function (e) { 
+
+
+        e.preventDefault();
+         var files = e.originalEvent.dataTransfer.files;
+
+         //We need to send dropped files to Server
+         handleFileUpload(files,id);
+	});
+
+	$(document).on('click', '.dragdrop', function (e) { 
+		id = $(this).attr("fundus_id");
+        $("#uploader_"+id).click();
+	});
+
+
+	$(document).on('click', '.upload_here', function (e) { 
+		id = $(this).attr("fundus_id");
+        $("#uploader_"+id).click();
+	});
+
+    $(".uploader").change(function() {
+    	id = $(this).attr("fundus_id");
+        handleFileUpload($("#uploader_"+id)[0].files,id);
+    });
+
+
+});
+
+
+
+
+function handleFileUpload(files,id) {
+
+    var data = new FormData();
+    $.each(files, function(i, file) {
+            data.append(file['name'], file);
+    });
+
+    $.ajax({
+           url: '/upload_eye',
+           type: "post",
+           dataType: "json",
+           data: data,
+           processData: false,
+           contentType: false,
+           beforeSend :function() {
+                
+            },
+            success: function(data, textStatus, jqXHR) {                
+                
+            	$("#dragdrop_"+id + " p").remove();
+            	$("#eye_img_"+id).attr("src",data['eye_path']);
+            	$("#eye_img_"+id).css("width","100%");
+            	$("#eye_img_"+id).css("height","100%");
+            	$("#eye_img_"+id).css("top","0px");
+            	$("#eye_img_"+id).css("position","relative");
+
+            	$("#note_title_"+id).text("Clinical Note :")
+
+            	if(data['optical_disk']){
+            		$("#optical_disk_"+id).text("Optical Disk : 정상범위");
+            	} else {
+            		$("#optical_disk_"+id).text("Optical Disk : 비정상");
+            	}
+
+            	if(data['vessel_ratio']){
+            		$("#vessel_ratio_"+id).text("Vessel Ratio : 정상범위");
+            	} else {
+            		$("#vessel_ratio_"+id).text("Vessel Ratio : 비정상");
+            	}
+
+            	if(data['vessel_ratio']){
+            		$("#bleeding_"+id).text("Bleeding : 정상범위");
+            	} else {
+            		$("#bleeding_"+id).text("Bleeding : 비정상");
+            	}
+
+            	if(data['retina']){
+            		$("#retina_"+id).text("Retina : 정상범위");
+            	} else {
+            		$("#retina_"+id).text("Retina : 비정상");
+            	}
+
+            	if(data['etc']){
+            		$("#etc_"+id).text("기타문제 : 없음");
+            	} else {
+            		$("#etc_"+id).text("기타문제 : 비정상");
+            	}
+
+            	$("#tab_"+id).text(data["fundus_name"]);
+
+
+
+            }, error: function(jqXHR, textStatus, errorThrown) {
+                location.reload();
+            },
+            timeout: 20000
+        });
+}
+
+
+
+
+
+
+
+
+
